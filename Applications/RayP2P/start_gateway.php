d73f551eb47d0a860b1b6746ab65b6363332ae2b
Linkec <linkec@live.com>
2018-08-06 15:08:00 +0800

    Add Files

---
+++ Applications/RayP2P/start_gateway.php
@@ -0,0 +1,60 @@
+<?php 
+/**
+ * This file is part of workerman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link http://www.workerman.net/
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+use \Workerman\Worker;
+use \Workerman\WebServer;
+use \GatewayWorker\Gateway;
+use \GatewayWorker\BusinessWorker;
+use \Workerman\Autoloader;
+
+// 自动加载类
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$context = array(
+    // 更多ssl选项请参考手册 http://php.net/manual/zh/context.ssl.php
+    'ssl' => array(
+        // 请使用绝对路径
+        'local_cert'                 => '/root/server.pem', // 也可以是crt文件
+        'local_pk'                   => '/root/server.key',
+        'verify_peer'                => false,
+        // 'allow_self_signed' => true, //如果是自签名证书需要开启此选项
+    )
+);
+
+// gateway 进程，这里使用Text协议，可以用telnet测试
+$gateway = new Gateway("websocket://0.0.0.0:443",$context);
+// gateway名称，status方便查看
+$gateway->name = 'RayP2P GateWay';
+// gateway进程数
+$gateway->count = 4;
+// 本机ip，分布式部署时使用内网ip
+$gateway->lanIp = '127.0.0.1';
+// 内部通讯起始端口，假如$gateway->count=4，起始端口为4000
+// 则一般会使用4000 4001 4002 4003 4个端口作为内部通讯端口 
+$gateway->startPort = 2900;
+// 服务注册地址
+$gateway->registerAddress = '127.0.0.1:1238';
+
+// 心跳间隔
+$gateway->pingInterval = 10;
+// RFC格式心跳
+$gateway->pingRfc = true;
+// 心跳无响应限制
+$gateway->pingNotResponseLimit = 0;
+
+// 如果不是在根目录启动，则运行runAll方法
+if(!defined('GLOBAL_START'))
+{
+    Worker::runAll();
+}
+
--- README.md
+++ README.md
@@ -12,22 +12,38 @@ Written in full pure PHP. High performance and support cluster.
 ## Install
 About enviroment, you can review the webpage from [Workerman](http://www.workerman.com "Workerman")
 
+Download programe.
+    git clone https://github.com/RayP2P/hlsjs-p2p-signaler
+
+## Configure
+
+###Full package
+Don't really need to change anything.
+You just need to change the ssl settings in Applications/RayP2P/start_gateway.php
+
+###Master
+Applications/RayP2P/start_gateway.php
+1.Change the ssl settings.
+2.Change the $gateway->lanIp to Lan interface IP.
+
+###Cluster
+Applications/RayP2P/start_businessworker.php
+1.Change $worker->registerAddress To Register IP.
+
+## Running as service
+
 1.Running in Debug mode.
 
-    git clone https://github.com/RayP2P/hlsjs-p2p-signaler
 	php hlsjs-p2p-signaler/start.php
 
 2.Running in Daemon mode.
 
-    git clone https://github.com/RayP2P/hlsjs-p2p-signaler
 	php hlsjs-p2p-signaler/start.php -d
 	
 3.Running in Master mode.(exclude bussniess worker)
 
-    git clone https://github.com/RayP2P/hlsjs-p2p-signaler
 	php hlsjs-p2p-signaler/master.php -d
 	
-4.Running in Cluster mode.
+4.Running in Cluster mode.(only bussniess worker)
 
-    git clone https://github.com/RayP2P/hlsjs-p2p-signaler
 	php hlsjs-p2p-signaler/cluster.php -d
